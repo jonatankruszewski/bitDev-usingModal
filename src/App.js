d2e5d82240d4d6a0000e102ac9a62c79ebe960cf
Jonatan Kruszewski <jonatankruszewski@gmail.com>
2021-11-17 14:37:14 -0300

    Showing modal

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import './App.css';
 import React, { useState, useEffect }
   from 'react';
 import { PrimaryButton } from '@jonakru/modal.ui.button'
-import { ModalConfirm } from '@jonakru/modal.ui.modal-confirm'
+import { Modal } from '@jonakru/modal.ui.modal'
+import { Card } from '@jonakru/modal.ui.card'
+import { ModalConfirm } from '@jonakru/modal.ui.modal-confirm';
+
 const App = () => {
   const [open, setOpen] = useState(false)
   const cancel = () => alert('Cancelled');
@@ -11,11 +14,16 @@ const App = () => {
   return (<>
     {open ? <PrimaryButton text="Hide modal" cb={() => setOpen(false)}></PrimaryButton> :
       <PrimaryButton text="Show modal" cb={() => setOpen(true)}></PrimaryButton>}
-    <ModalConfirm show={open}
-      onConfirm={cnfirm} backDrop={true} clickAway={false} onCancel={cancel}>
-      <div>Hello I am a child</div>
+    <ModalConfirm
+      show={open}
+      backDrop={false}
+      clickAway={false}
+      onConfirm={cnfirm}
+      showCancel={false}
+      closeModal={() => setOpen(false)}
+    >
+      <Card title={"Hello world"} subTitle={"Testing component"} text="Lorem ipsum dorom"></Card>
     </ModalConfirm>
-
   </>
   )
 }
